44414e1d5ed223e497eabdd902ccc4a8854fca3c
gigamash <pexzaza6727@gmail.com>
2024-07-27 16:00:00 +0700

    uiu

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
     public static void main(String[] args) {
         MainPage main = new MainPage();
         
-        main.showMain();//555
+        main.showMain();
         //1234563
     }
 }
